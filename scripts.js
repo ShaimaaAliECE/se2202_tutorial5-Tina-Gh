5cd25aceb28bd2e897851f744ac7e65e2375d784
Tina Gholami <44522127+Tina-Gh@users.noreply.github.com>
2021-10-20 21:01:55 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the gam
 //initialize the game
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
+lbl = document. getElementById('next-lbl');
+lbl.innerText = nextPlayer;
 
 
 //This call will create the buttons needed for the gameboard.
@@ -11,6 +13,11 @@ createGameBoard()
 function createGameBoard()
 {
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
+    let cells = document.querySelectorAll('td');
+    for (let i=0; i<cells.length; i++)
+    {
+        cells[i].innerHTML = '<button> [ ] </button>'
+    }
    
 }
 
@@ -27,13 +34,26 @@ function takeCell(event)
     /*
         When the button is clicked, the space inside its square brackets is replaced by the value in the nextPlayer before switching it
     */
+    let clickedButton = event.target;
+    clickedButton.innerText = '[' + nextPlayer + ']';
+    if (nextPlayer == 'X')
+    {
+        nextPlayer = 'O'
+    }
+    else
+    {
+        nextPlayer = 'X'
+    }
+    lbl.innerText = nextPlayer;
 
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
-
+    clickedButton.disabled = true;
+    
     // Check if the game is over
     if (isGameOver())
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        document.getElementById('game-over-lbl').innerHTML = '<h1> Game Over </h1>' 
     }
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
@@ -42,5 +62,11 @@ function takeCell(event)
 function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
-   
+    for (let i=0; i<btns.length; i++)
+    {
+        if (btns[i].disabled == true)
+            return true;
+        else
+            return false;
+    }
 }
